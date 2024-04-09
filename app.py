af72a098737654540615ef3166084cf9125d1db9
Simon Clark <simon.clark@sintef.no>
2024-04-09 02:24:48 +0200

    add context to json-ld

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
+import plotly.graph_objects as go
 
 # Function to parse the date string to datetime
 def parse_date(date):
@@ -9,24 +9,31 @@ def parse_date(date):
 # Function to plot the cumulative sum of Mass
 def plot_cumulative_mass(df):
     df['Date'] = df['Date / YYYY-MM-DD'].apply(parse_date)
-    df['Cumulative Mass'] = df['Mass / kg'].cumsum()
-    fig = px.line(df, x='Date', y='Cumulative Mass', title='Cumulative Sum of Mass Over Time')
+    df['Total Mass'] = df['Mass / kg'].cumsum()
+    # Create figure
+    fig = go.Figure()
+    
+    # Add line trace
+    fig.add_trace(go.Scatter(x=df['Date'], y=df['Total Mass'], mode='lines', name='Total Mass'))
+    
+    # Add filled area trace
+    fig.add_trace(go.Scatter(x=df['Date'], y=df['Total Mass'], fill='tozeroy', mode='none', fillcolor='rgba(0,100,80,0.2)', name=""))
+    
+    # Update layout
+    fig.update_layout(title='Total Mass of Picked Litter', xaxis_title='Date', yaxis_title='Total Mass  /  kg')
+    
     st.plotly_chart(fig)
 
 # Streamlit app
 def main():
-    st.title('Cumulative Sum of Mass')
+    st.title('Trondheim Trash Data Hub')
     
     # Read data from CSV file
-    url = 'https://raw.githubusercontent.com/jsimonclark/TrondheimTrash/main/data/MassData.tsv'
-    df = pd.read_csv(url, sep='\s{3,}', engine='python') 
-    
-    # Display raw data
-    st.subheader('Raw Data')
-    st.write(df)
+    url = 'https://raw.githubusercontent.com/jsimonclark/TrondheimTrash/main/data/MassData.csv'
+    df = pd.read_csv(url) 
     
     # Plot cumulative sum of Mass
-    st.subheader('Cumulative Sum of Mass')
+    #st.subheader('Cumulative Sum of Mass')
     plot_cumulative_mass(df)
 
 if __name__ == "__main__":
